684b4312044ebb95560061e96e5ae3e80bff7288
Mahmoud Darwish <meywd@msn.com>
2026-01-03 00:19:18 -0500

    Initial Commit

---
+++ LogViewer/LogViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LogViewerApp.swift
+//  LogViewer
+//
+//  Created by Mahmoud Darwish on 1/3/26.
+//
+
+import SwiftUI
+
+@main
+struct LogViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
